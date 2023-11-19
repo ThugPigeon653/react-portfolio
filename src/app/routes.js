f342d1ac5c2a12a3a7ab8c9eee30e31dfa808768
ThugPigeon653 <89567974+ThugPigeon653@users.noreply.github.com>
2023-11-19 12:02:26 +1000

    fixed routing issue (404 on refresh)

--- src/app/routes.js
+++ src/app/routes.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { Route, Routes} from "react-router-dom";
-import withRouter from "../hooks/withRouter"
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import withRouter from "../hooks/withRouter";
 import { Home } from "../pages/home";
 import { Portfolio } from "../pages/portfolio";
 import { ContactUs } from "../pages/contact";
@@ -20,11 +20,10 @@ const AnimatedRoutes = withRouter(({ location }) => (
       unmountOnExit
     >
       <Routes location={location}>
-        <Route exact path="/" element={<Home />} />
+        <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/portfolio" element={<Portfolio />} />
         <Route path="/contact" element={<ContactUs />} />
-        <Route path="*" element={<Home />} />
       </Routes>
     </CSSTransition>
   </TransitionGroup>
